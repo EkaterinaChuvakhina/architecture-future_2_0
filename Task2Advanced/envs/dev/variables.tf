40ea5c093968b3757273b3024af264c72f758a0b
echuvakhina <echuvakhina@mail.ru>
2026-04-14 15:43:32 +0700

    Task1-2

--- Task2Advanced/envs/dev/variables.tf
+++ Task2Advanced/envs/dev/variables.tf
@@ -12,18 +12,3 @@ variable "subnet_id" { type = string }
 variable "ssh_key" { type = string }
 variable "zone" { type = string }
 variable "image_id" { type = string }
-variable "minio_endpoint" {
-  description = "Endpoint Minio / Yandex Object Storage"
-  type        = string
-}
-variable "minio_access_key" {
-  description = "Access Key для MinIO"
-  type        = string
-  sensitive   = true
-}
-
-variable "minio_secret_key" {
-  description = "Secret Key для MinIO"
-  type        = string
-  sensitive   = true
-}
--- Task2Advanced/envs/stage/variables.tf
+++ Task2Advanced/envs/stage/variables.tf
@@ -11,19 +11,4 @@ variable "disk_size" { type = number }
 variable "subnet_id" { type = string }
 variable "ssh_key" { type = string }
 variable "zone" { type = string }
-variable "image_id" { type = string }
-variable "minio_endpoint" {
-  description = "Endpoint Minio / Yandex Object Storage"
-  type        = string
-}
-variable "minio_access_key" {
-  description = "Access Key для MinIO"
-  type        = string
-  sensitive   = true
-}
-
-variable "minio_secret_key" {
-  description = "Secret Key для MinIO"
-  type        = string
-  sensitive   = true
-}
+variable "image_id" { type = string }k
