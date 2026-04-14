5c3ed5b878c9df2f6833a9173678e7eff6389101
echuvakhina <echuvakhina@mail.ru>
2026-04-14 14:53:22 +0700

    Task1-2

--- Task2Advanced/envs/dev/backend.tf
+++ Task2Advanced/envs/dev/backend.tf
@@ -3,10 +3,15 @@ terraform {
     bucket                      = "future-tfstate"
     key                         = "task2advanced/dev/terraform.tfstate"
     region                      = "ru-central1"
-    endpoint                    = "https://storage.yandexcloud.net"
+
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
+    skip_requesting_account_id  = true     # ← это важно!
     force_path_style            = true
     encrypt                     = true
   }
